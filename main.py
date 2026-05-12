8b3c6504a96569312f9c23346fbb000b6dea5088
eyesmanking-ops <eyesmanking@gmail.com>
2026-05-12 13:00:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ def main():
     # 採用零件拼湊法，徹底避開系統縮網址問題
     # 程式執行時會自動組合成：https:// + 網域 + 路徑
     SOURCES = {
-        "中央社": "https://feedburner.com/rsscna/mainland",
+        "中央社": "https://feeds.feedburner.com/rsscna/mainland",
         "自由時報": "https://news.ltn.com.tw/rss/all.xml",
-        "中時新聞": "https://chinatimes.com/rss/realtimenews-total.xml",
+        "中時新聞": "https://www.chinatimes.com/rss/realtimenews-total.xml",
         "聯合新聞": "https://udn.com/rssfeed/news/2/6638?ch=news",
-        "青年日報": "https://ydn.com.tw/rss/news/1",
+        "青年日報": "https://www.ydn.com.tw/rss/news/1",
         "教育電台": "https://www.ner.gov.tw/rss"
     }
 
